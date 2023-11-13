bcb2a4403d148425ad37310c4c195cc455c6b658
Derblaz Barbosa <derblaz@gmail.com>
2023-11-13 16:48:44 -0300

    build: Configurado Infraestrutura com Spring

---
+++ infrastructure/src/main/java/com/derblaz/educational/institution/api/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.derblaz.educational.institution.api.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.derblaz.educational.institution.api")
+public class WebServerConfig {
+}
